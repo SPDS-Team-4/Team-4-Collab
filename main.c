d4a54a01e9ec5642eecdac77e33efae46559e58a
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-18 19:18:00 -0400

    Update main.c

--- main.c
+++ main.c
@@ -18,41 +18,41 @@ int main() {
         printf("\n");
         printf("%s : ", KEY);
  
-        scanf("%c", operand);
+        scanf("%c", &operand);
  
         switch(operand)
         {
-            case '+': scanf(" %d %d\n",a,b);
+            case '+': scanf(" %d %d\n",&a,&b);
                       x = add(a,b);
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;
  
-            case '-': scanf(" %d %d\n",a,b);
+            case '-': scanf(" %d %d\n",&a,&b);
                       x = sub(a,b);
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;
  
-            case '*': scanf(" %d %d\n",a,b);
-                      x = multiply(a,b);
+            case '*': scanf(" %d %d\n",&a,&b);
+                      x = multiply(&a,&b);
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;
  
-            case '/': scanf(" %d %d\n",a,b);
+            case '/': scanf(" %d %d\n",&a,&b);
                       x = division(a,b);
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;
  
-            case '?': scanf(" %d %d\n",a,b);
+            case '?': scanf(" %d %d\n",&a,&b);
                       x = mod(a,b);
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;
  
-            case '!': scanf(" %d\n",a);
+            case '!': scanf(" %d\n",&a);
                       x = factorial(a);
                       printf("%d%c = %d",a,operand,x);
                       break;
  
-            case '^': scanf(" %d %d\n",a,b);
+            case '^': scanf(" %d %d\n",&a,&b);
                       x = power();
                       printf("%d %c %d = %d",a,operand,b,x);
                       break;

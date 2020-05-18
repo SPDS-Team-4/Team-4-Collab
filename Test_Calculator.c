aa2588de3a24d45beac24139754159e6c7c95fd9
Seema-S-123 <65356384+Seema-S-123@users.noreply.github.com>
2020-05-18 18:24:48 -0500

    Update Test_Calculator.c
    Updated the test script for correct function names, and redeclaration of suite done

--- Test_Calculator.c
+++ Test_Calculator.c
@@ -13,17 +13,17 @@ void test_add(void)
 
 void test_subtract(void)
 {
-	CU_ASSERT ( 1 == subtract(10,9));
+	CU_ASSERT ( 1 == sub(10,9));
     
 }
 
 int main() 
 {
 CU_initialize_registry();
-CU_pSuite suite = CU_add_suite("TestSuite_1", 0, 0);
-CU_pSuite suite = CU_add_suite("TestSuite_2", 0, 0);
-CU_add_test(suite, "Test_Suite_1", test_add);
-CU_add_test(suite, "Test_Suite_2", test_subtract);
+CU_pSuite suite_1 = CU_add_suite("TestSuite_1", 0, 0);
+CU_pSuite suite_2 = CU_add_suite("TestSuite_2", 0, 0);
+CU_add_test(suite_1, "Test_Suite_1", test_add);
+CU_add_test(suite_2, "Test_Suite_2", test_subtract);
 CU_basic_set_mode(CU_BRM_VERBOSE);
 CU_basic_run_tests();
 CU_cleanup_registry();

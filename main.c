a30ed822330b8345912bbbf08c0bd64c357bd437
Anuj Murali <40844059+AnujMurali@users.noreply.github.com>
2020-05-18 14:52:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main() {
   printf ("Add: %d\n",add(a,b));
   printf ("Subtract: %d\n",sub(a,b));
   printf ("Multiply: %d\n",multiply(a,b));
+  printf ("Sub2: %d\n",subtract(a,b));
   
   return 0;
  }

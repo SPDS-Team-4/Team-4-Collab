c6246e9b816de2814d8180b53af9f319ba407106
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-26 13:57:41 -0400

    Update read_ints.c

--- read_ints.c
+++ read_ints.c
@@ -6,7 +6,7 @@
 *   read an unknown number of intgers from the command line into an array
 *   returns the last integer (will be updated to return a list)
 */
-int* read_ints(){
+int read_ints(){
   int k;
   while (scanf("%d", &k) == 1) {
   }

ac2b7669c101cb3a3e87e884605e974de1f1cb51
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-26 13:53:05 -0400

    Create read_ints.c

---
+++ read_ints.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "collab.h"
+
+/*
+* read_ints()
+*   read an unknown number of intgers from the command line into an array
+*   returns the last integer (will be updated to return a list)
+*/
+int* read_ints(){
+  int k;
+  while (scanf("%d", &k) == 1) {
+  }
+  return k;
+}
+}

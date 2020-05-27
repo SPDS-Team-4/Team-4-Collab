d31429bd0ad33c11f80ddaf607ced222ac755d85
GregoryTyree <65422917+GregoryTyree@users.noreply.github.com>
2020-05-27 14:01:08 -0400

    Update collab.h
    added IO_list structure

--- collab.h
+++ collab.h
@@ -15,3 +15,12 @@ int power ();
 void helpMenu ();
 int read_ints();
 #endif
+
+struct IO_list {
+  char operator;                //character representation of operation
+  void (*func)(IO_list *op);    //function pointer for performing opertation
+  int inLimit;                  //length of the input array
+  int *input;                   //pointer to the input array
+  int outLimit;                 //length of the output array
+  int *output;                  //pointer to the output array
+}

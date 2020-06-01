174d1cdba0434c88027be733060a52aaf34c333e
Seema-S-123 <65356384+Seema-S-123@users.noreply.github.com>
2020-06-01 17:19:53 -0500

    Update Test_Calculator.c
    Added a test function to include test data for power function implementation that takes n inputs . To do: The assert or test syntax in CUnit test for the structure variable check

--- Test_Calculator.c
+++ Test_Calculator.c
@@ -30,15 +30,35 @@ void test_operations(void)
 	CU_ASSERT (power(var1,var2) == 100000);
         CU_ASSERT (power(10,2) == 100);
 }
+
+void test_power_n_inputs(void)
+{
+//the header collab.h will have the defintion of the structure
+	struct Input_Output_list test_input_struct, test_output_struct; 
+	int in_array[5] =  {10,2,3,4,5};
+	int out_array[4] =  {100,1000,10000,100000};	
+	test_input_struct.operation = Power;
+	test_input_struct.inLimit = 5;
+	test_input_struct.input = in_array;
+	test_output_struct = test_input_struct;	
+	test_output_struct.outLimit = 4;
+	test_output_struct.outLimit = out_array;	
+	//Compare the structure variable that is updated by the function power_n_inpouts
+	//to the expected structure test_output_struct
+	CU_TSt_PRT_EQUAL (  ,);
+    
+}
 int main() 
 {
 CU_initialize_registry();
 CU_pSuite suite_1 = CU_add_suite("TestSuite_1", 0, 0);
 CU_pSuite suite_2 = CU_add_suite("TestSuite_2", 0, 0);
 CU_pSuite suite_3 = CU_add_suite("TestSuite_3", 0, 0);
+CU_pSuite suite_4 = CU_add_suite("TestSuite_4", 0, 0);	
 CU_add_test(suite_1, "Test_Suite_1", test_add);
 CU_add_test(suite_2, "Test_Suite_2", test_subtract);
 CU_add_test(suite_3, "Test_Suite_3", test_operations);
+CU_add_test(suite_4, "Test_Suite_4", test_power_n_inputs);
 CU_basic_set_mode(CU_BRM_VERBOSE);
 CU_basic_run_tests();
 CU_cleanup_registry();
